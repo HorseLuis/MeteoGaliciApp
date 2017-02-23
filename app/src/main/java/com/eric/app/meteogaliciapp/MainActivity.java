c78c0effb43d11f1223da8a05ae1c86983b54e1f
Eric Vazquez <galletobalu@gmail.com>
2017-02-23 23:29:29 +0100

    Cambio del JSON de fuente. Soporte a toda Galicia. Nuevos iconos para el estado del cielo. Nuevo texto para el estado del cielo. Rediseño de la interfaz.

--- app/src/main/java/com/eric/app/meteogaliciapp/MainActivity.java
+++ app/src/main/java/com/eric/app/meteogaliciapp/MainActivity.java
@@ -72,16 +72,16 @@ import javax.xml.transform.Result;
 import static android.R.attr.category;
 
 public class MainActivity extends AppCompatActivity {
-    String pag = "http://servizos.meteogalicia.gal/rss/observacion/estadoEstacionsMeteo.action?idEst=10148";
+    String pag = "http://servizos.meteogalicia.gal/rss/observacion/observacionConcellos.action";
     String estadoCielo;
-    String varTemperatura;
-    String temperatura;
+    double sensTermica;
+    double temperatura;
     String concello;
 
-    private static final String TAG_CIELO = "lnIconoCeo";
-    private static final String TAG_VAR = "lnIconoTemperatura";
-    private static final String TAG_TEMP = "valorTemperatura";
-    private static final String TAG_CITY = "concello";
+    private static final String TAG_CIELO = "icoEstadoCeo";
+    private static final String TAG_VAR = "sensacionTermica";
+    private static final String TAG_TEMP = "temperatura";
+    private static final String TAG_CITY = "nomeConcello";
 
     JSONArray datos = null;
 
@@ -123,91 +123,231 @@ public class MainActivity extends AppCompatActivity {
             pDialog.dismiss();
             try {
                 // Getting JSON Array
-                datos = json.getJSONArray("listEstadoActual");
-                JSONObject c = datos.getJSONObject(0);
+                datos = json.getJSONArray("listaObservacionConcellos");
+                for (int i=0;i<datos.length();i++) {
+                    JSONObject c = datos.getJSONObject(i);
 
-                // Storing  JSON item in a Variable
-                String cielo = c.getString(TAG_CIELO);
-                String var = c.getString(TAG_VAR);
-                String temp = c.getString(TAG_TEMP);
-                String city = c.getString(TAG_CITY);
+                    // Storing  JSON item in a Variable
+                    String city = c.getString(TAG_CITY);
+                    if (city.equals("Ourense")){
+                        String cielo = c.getString(TAG_CIELO);
+                        double var = c.getDouble(TAG_VAR);
+                        double temp = c.getDouble(TAG_TEMP);
 
-                //Set JSON Data in TextView
-                estadoCielo = cielo;
-                varTemperatura = var;
-                temperatura = temp;
-                concello = city;
 
-                TextView view_temp = (TextView) findViewById(R.id.text_temperatura);
-                view_temp.setText(temperatura+"º");
-                TextView view_city = (TextView) findViewById(R.id.text_ciudad);
-                view_city.setText(concello);
+                        //Set JSON Data in TextView
+                        estadoCielo = cielo;
+                        sensTermica = var;
+                        temperatura = temp;
+                        concello = city;
 
-                ImageView view_estado = (ImageView) findViewById(R.id.estado_cielo);
-                TextView dato_cielo = (TextView) findViewById(R.id.dato_cielo);
-                int imgres = 0;
-                String datasky = "";
-                switch (estadoCielo){
-                    case "101":
-                        imgres = R.drawable.icono_despejado;
-                        datasky = "DESPEJADO";
-                        break;
-                    case "103":
-                        imgres = R.drawable.icono_nubes_claros;
-                        datasky = "NUBES Y CLAROS";
-                        break;
-                    case "105":
-                        imgres = R.drawable.icono_nublado;
-                        datasky = "NUBLADO";
-                        break;
-                    case "107":
-                        imgres = R.drawable.icono_chubascos;
-                        datasky = "CHUBASCOS";
-                        break;
-                    case "111":
-                        imgres = R.drawable.icono_lluvia;
-                        datasky = "LLUVIA";
-                        break;
-                    case "201":
-                        imgres = R.drawable.icono_noche_despejada;
-                        datasky = "DESPEJADO";
-                        break;
-                    case "211":
-                        imgres = R.drawable.icono_noche_luvia;
-                        datasky = "LLUVIA";
-                        break;
-                    case "-9999":
-                        imgres = R.drawable.icono_void;
-                        datasky = "SIN DATOS";
-                        break;
+                        TextView view_temp = (TextView) findViewById(R.id.text_temperatura);
+                        view_temp.setText(temperatura+"º");
+                        TextView view_city = (TextView) findViewById(R.id.text_ciudad);
+                        view_city.setText(concello);
+
+                        ImageView view_estado = (ImageView) findViewById(R.id.estado_cielo);
+                        TextView dato_cielo = (TextView) findViewById(R.id.dato_cielo);
+                        int imgres = 0;
+                        String datasky = "";
+                        switch (estadoCielo){
+                            case "101":
+                                imgres = R.drawable.dia_despejado;
+                                datasky = "DESPEXADO";
+                                break;
+                            case "102":
+                                imgres = R.drawable.dia_nubesaltas;
+                                datasky = "NUBES ALTAS";
+                                break;
+                            case "103":
+                                imgres = R.drawable.dia_nubesclaros;
+                                datasky = "NUBES E CLAROS";
+                                break;
+                            case "104":
+                                imgres = R.drawable.dia_nublado75;
+                                datasky = "NUBES 75%";
+                                break;
+                            case "105":
+                                imgres = R.drawable.nublado;
+                                datasky = "CUBERTO";
+                                break;
+                            case "106":
+                                imgres = R.drawable.niebla;
+                                datasky = "NEBOA";
+                                break;
+                            case "107":
+                                imgres = R.drawable.chuvasco;
+                                datasky = "CHUVASCO";
+                                break;
+                            case "108":
+                                imgres = R.drawable.chuvasco75;
+                                datasky = "CHUVASCO 75%";
+                                break;
+                            case "109":
+                                imgres = R.drawable.aguanieve;
+                                datasky = "CHUVASCO NEVE";
+                                break;
+                            case "110":
+                                imgres = R.drawable.orballo;
+                                datasky = "ORBALLO";
+                                break;
+                            case "111":
+                                imgres = R.drawable.lluvia;
+                                datasky = "CHOIVA";
+                                break;
+                            case "112":
+                                imgres = R.drawable.nieve;
+                                datasky = "NEVE";
+                                break;
+                            case "113":
+                                imgres = R.drawable.tormenta;
+                                datasky = "TREBOADA";
+                                break;
+                            case "114":
+                                imgres = R.drawable.neblina;
+                                datasky = "BRETEMA";
+                                break;
+                            case "115":
+                                imgres = R.drawable.niebla;
+                                datasky = "NEBOA";
+                                break;
+                            case "116":
+                                imgres = R.drawable.nubemedia;
+                                datasky = "NUBES MEDIAS";
+                                break;
+                            case "117":
+                                imgres = R.drawable.orballo;
+                                datasky = "CHOIVA DEBIL";
+                                break;
+                            case "118":
+                                imgres = R.drawable.chuvasco_debil;
+                                datasky = "CHUVASCOS DEBILES";
+                                break;
+                            case "119":
+                                imgres = R.drawable.tormenta;
+                                datasky = "TREBOADA POUCAS NUBES";
+                                break;
+                            case "120":
+                                imgres = R.drawable.aguanieve;
+                                datasky = "AUGA NEVE";
+                                break;
+                            case "121":
+                                imgres = R.drawable.ventisca;
+                                datasky = "SARABIA";
+                                break;
+
+
+                            case "201":
+                                imgres = R.drawable.noche_despejado;
+                                datasky = "DESPEXADO";
+                                break;
+                            case "202":
+                                imgres = R.drawable.noche_nubemedia;
+                                datasky = "NUBES ALTAS";
+                                break;
+                            case "203":
+                                imgres = R.drawable.noche_nubemedia;
+                                datasky = "NUBES E CLAROS";
+                                break;
+                            case "204":
+                                imgres = R.drawable.noche_nubemedia;
+                                datasky = "NUBES 75%";
+                                break;
+                            case "205":
+                                imgres = R.drawable.nublado;
+                                datasky = "CUBERTO";
+                                break;
+                            case "206":
+                                imgres = R.drawable.niebla;
+                                datasky = "NEBOA";
+                                break;
+                            case "207":
+                                imgres = R.drawable.chuvasco;
+                                datasky = "CHUVASCO";
+                                break;
+                            case "208":
+                                imgres = R.drawable.noche_lluvianubes;
+                                datasky = "CHUVASCO 75%";
+                                break;
+                            case "209":
+                                imgres = R.drawable.aguanieve;
+                                datasky = "CHUVASCO NEVE";
+                                break;
+                            case "210":
+                                imgres = R.drawable.orballo;
+                                datasky = "ORBALLO";
+                                break;
+                            case "211":
+                                imgres = R.drawable.noche_lluvia;
+                                datasky = "CHOIVA";
+                                break;
+                            case "212":
+                                imgres = R.drawable.noche_nieve;
+                                datasky = "NEVE";
+                                break;
+                            case "213":
+                                imgres = R.drawable.tormenta;
+                                datasky = "TREBOADA";
+                                break;
+                            case "214":
+                                imgres = R.drawable.neblina;
+                                datasky = "BRETEMA";
+                                break;
+                            case "215":
+                                imgres = R.drawable.niebla;
+                                datasky = "NEBOA";
+                                break;
+                            case "216":
+                                imgres = R.drawable.noche_nubemedia;
+                                datasky = "NUBES MEDIAS";
+                                break;
+                            case "217":
+                                imgres = R.drawable.noche_lluvianubes;
+                                datasky = "CHOIVA DEBIL";
+                                break;
+                            case "218":
+                                imgres = R.drawable.noche_lluvianubes;
+                                datasky = "CHUVASCOS DEBILES";
+                                break;
+                            case "219":
+                                imgres = R.drawable.tormenta;
+                                datasky = "TREBOADA POUCAS NUBES";
+                                break;
+                            case "220":
+                                imgres = R.drawable.aguanieve;
+                                datasky = "AUGA NEVE";
+                                break;
+                            case "221":
+                                imgres = R.drawable.ventisca;
+                                datasky = "SARABIA";
+                                break;
+
+
+                            case "-9999":
+                                imgres = R.drawable.icono_void;
+                                datasky = "NO DISPONIBLE";
+                                break;
+                        }
+                        view_estado.setImageResource(imgres);
+                        dato_cielo.setText(datasky);
+
+                        ImageView view_var = (ImageView) findViewById(R.id.tendencia_temperatura);
+                        int imgres1 = 0;
+                        if (sensTermica<temperatura){
+                            imgres1 = R.drawable.icono_temp_baja;
+                        }
+                        else if (sensTermica>temperatura){
+                            imgres1 = R.drawable.icono_temp_sube;
+                        }
+                        else {
+                            imgres1 = R.drawable.icono_temp_estable;
+                        }
+                        view_var.setImageResource(imgres1);
+                    }
+
                 }
-                view_estado.setImageResource(imgres);
-                dato_cielo.setText(datasky);
 
-                ImageView view_var = (ImageView) findViewById(R.id.tendencia_temperatura);
-                TextView dato_temp = (TextView) findViewById(R.id.dato_temp);
-                int imgres1 = 0;
-                String datatemp = "";
-                switch (varTemperatura){
-                    case "400":
-                        imgres1 = R.drawable.icono_temp_baja;
-                        datatemp = "Temperaturas en descenso";
-                        break;
-                    case "401":
-                        imgres1 = R.drawable.icono_temp_estable;
-                        datatemp = "Temperaturas estables";
-                        break;
-                    case "402":
-                        imgres1 = R.drawable.icono_temp_sube;
-                        datatemp = "Temperaturas en aumento";
-                        break;
-                    case "-9999":
-                        imgres1 = R.drawable.icono_void;
-                        datatemp = "Variación de temperatura no disponible";
-                        break;
-                }
-                view_var.setImageResource(imgres1);
-                dato_temp.setText(datatemp);
+
 
             } catch (JSONException e) {
                 e.printStackTrace();
